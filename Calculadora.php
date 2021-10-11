9f1f8af791181d797223d900513feebcd451aafa
Iván <ivanflori31@gmail.com>
2021-10-11 10:46:41 +0200

    claculadora

---
+++ Calculadora.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=+, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+<H1>Calculadora</H1>
+
+<form>
+    <fieldset>
+    <label for="Nombre">numero1 <input type="text"> </label><br>
+    <label for="Nombre">numero2<input type="text"> </label><br>
+
+
+    <label for="Nombre"> <input type="button" name= "" value="+"> </label><br>
+        
+        <label for="Nombre"><input type="button" name= "" value="-"> </label><br>
+        <label for="Nombre"><input type="button" name= "" value="*"> </label><br>
+        <label for="Nombre"><input type="button" name= "" value="/"> </label><br>
+        <label for="Nombre"><input type="button" name= "" value="%"> </label><br>
+        
+
+
+
+
+</fieldset>
+
+<input type="submit"> <br>
+
+
+
+
+
+</form>
+
+</body>
+</html>
